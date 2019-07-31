a8055a54605c32485cd9b55d75412a749c1d8671
Dan Dash <ddash@ciphertechsolutions.com>
2019-07-31 08:57:14 -0400

    Add check to ensure end_page_offset is intialized and avoid TypeError on + if it is None

--- kordesii/utils/function_tracing/memory.py
+++ kordesii/utils/function_tracing/memory.py
@@ -269,7 +269,7 @@ class Memory(object):
         _end = self.PAGE_SIZE + len(value) - 1
         if end and end_page_index == page_index + 1:
             _end = min(_end, self.PAGE_SIZE + end_page_offset)
-        if _end == self.PAGE_SIZE + end_page_offset and _end <= _start:
+        if end_page_offset is not None and _end == self.PAGE_SIZE + end_page_offset and _end <= _start:
             return -1
 
         next_page = self._pages.get(page_index + 1, bytearray(self.PAGE_SIZE))
